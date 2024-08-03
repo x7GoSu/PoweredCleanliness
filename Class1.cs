26b6a0dc4da8d63f98b453cbc60e2d7445d53c80
x7GoSu <joshjmilam@gmail.com>
2024-08-03 13:15:25 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using PoweredCleanliness;
+using Verse;
+
+public class CompProperties_PowerCleanliness : CompProperties
+{
+    public CompProperties_PowerCleanliness()
+    {
+        compClass = typeof(CompPowerCleanliness);
+    }
+}
