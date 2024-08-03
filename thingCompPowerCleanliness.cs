a8db9fa6b1384e380ae0e6787b89583370f07a68
x7GoSu <joshjmilam@gmail.com>
2024-08-03 15:20:32 -0700

    Update thingCompPowerCleanliness.cs

--- thingCompPowerCleanliness.cs
+++ thingCompPowerCleanliness.cs
@@ -1,4 +1,4 @@
-﻿using RimWorld;
+using RimWorld;
 using Verse;
 
 namespace PoweredCleanliness
@@ -18,21 +18,6 @@ namespace PoweredCleanliness
         public CompProperties_PowerCleanliness Props => (CompProperties_PowerCleanliness)props;
 
         private float originalCleanlinessValue;
-        private bool initialSetupDone = false;
-
-        public override void PostSpawnSetup(bool respawningAfterLoad)
-        {
-            base.PostSpawnSetup(respawningAfterLoad);
-            if (!initialSetupDone)
-            {
-                // Save the original cleanliness value
-                StatModifier cleanlinessModifier = parent.def.statBases.Find(s => s.stat == StatDefOf.Cleanliness);
-                originalCleanlinessValue = cleanlinessModifier != null ? cleanlinessModifier.value : 0.0f;
-                Log.Message($"Initial Cleanliness Value for {parent.Label}: {originalCleanlinessValue}");
-                initialSetupDone = true;
-            }
-            UpdateCleanliness();
-        }
 
         public override void CompTick()
         {
@@ -59,7 +44,7 @@ namespace PoweredCleanliness
         {
             Log.Message($"Setting cleanliness for {parent.Label} to {value}");
             StatModifier cleanlinessModifier = parent.def.statBases.Find(s => s.stat == StatDefOf.Cleanliness);
-
+            StatModifier cleanlinessremove = parent.def.statBases.Find(s => s.stat == StatDefOf.Cleanliness);
             if (cleanlinessModifier == null)
             {
                 cleanlinessModifier = new StatModifier
@@ -75,42 +60,7 @@ namespace PoweredCleanliness
                 cleanlinessModifier.value = value;
                 Log.Message($"Updated cleanliness modifier for {parent.Label} to value {value}");
             }
-
-            // Notify the game to update the stats
-            if (parent.Spawned)
-            {
-                parent.Map.mapDrawer.MapMeshDirty(parent.Position, MapMeshFlagDefOf.Things);
-                parent.Map.mapDrawer.MapMeshDirty(parent.Position, MapMeshFlagDefOf.Buildings);
-                Log.Message($"Building {parent.Label} updated buildings");
-
-                // Notify the room to update its stats
-                Room room = parent.GetRoom();
-                if (room != null)
-                {
-                    room.Notify_TerrainChanged();
-                    Log.Message($"Notified room to update cleanliness");
-                }
-            }
         }
 
-        public override void PostDeSpawn(Map map)
-        {
-            base.PostDeSpawn(map);
-            // Revert cleanliness value to original when the building is despawned
-            SetCleanliness(originalCleanlinessValue);
-        }
-
-        public override string CompInspectStringExtra()
-        {
-            StatModifier cleanlinessModifier = parent.def.statBases.Find(s => s.stat == StatDefOf.Cleanliness);
-            float currentCleanliness = cleanlinessModifier != null ? cleanlinessModifier.value : 0.0f;
-            return $"Cleanliness: {currentCleanliness}";
-        }
-
-        public override void PostExposeData()
-        {
-            base.PostExposeData();
-            Scribe_Values.Look(ref originalCleanlinessValue, "originalCleanlinessValue", 0.0f);
-        }
     }
 }
